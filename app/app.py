b78455e1bce372a0989be26f9cdb7c9efeaaa01f
droxbrl <rustanovich01@gmail.com>
2023-02-05 00:32:12 +0400

    migrations added

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Configuration
-
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
-
+migrate = Migrate(app=app, db=db)
+# импорт нужен для миграций
+from models import *
