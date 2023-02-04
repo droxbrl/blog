290e9b5aa3caf69d2421dea0f65d001e60a3647c
droxbrl <rustanovich01@gmail.com>
2023-02-04 21:07:48 +0400

    //

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Configuration
 from posts.blueprint import posts
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db = SQLAlchemy(app)
 app.register_blueprint(blueprint=posts, url_prefix='/blog')
